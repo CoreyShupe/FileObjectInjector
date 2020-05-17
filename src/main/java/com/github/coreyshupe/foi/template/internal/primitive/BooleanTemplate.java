585d018dfdfc522bc3f4ad5da53b890f9b99341c
CoreyShupe101 <coreyshupe101@gmail.com>
2020-05-16 23:11:02 -0400

    Added Boolean template.

---
+++ src/main/java/com/github/coreyshupe/foi/template/internal/primitive/BooleanTemplate.java
@@ -0,0 +1,31 @@
+package com.github.coreyshupe.foi.template.internal.primitive;
+
+import com.github.coreyshupe.foi.template.SizedTemplate;
+import com.github.coreyshupe.foi.template.internal.CollectionTemplate;
+import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
+
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+
+public class BooleanTemplate extends SizedTemplate<Boolean> {
+    @Getter private final static BooleanTemplate instance = new BooleanTemplate();
+    @Getter
+    private final static CollectionTemplate<Boolean> collectionInstance = new CollectionTemplate<>(Boolean.class, instance, ArrayList::new);
+
+    public BooleanTemplate() {
+        super(Boolean.class, Byte.SIZE);
+    }
+
+    @Override public boolean isThis(@NotNull Class<?> givenType) {
+        return super.isThis(givenType) || givenType == boolean.class;
+    }
+
+    @NotNull @Override public Boolean read(ByteBuffer buffer) {
+        return buffer.get() != 0x0;
+    }
+
+    @Override public void writeToBuffer(@NotNull Boolean object, @NotNull ByteBuffer buffer) {
+        buffer.put((byte) (object ? 0x1 : 0x0));
+    }
+}
